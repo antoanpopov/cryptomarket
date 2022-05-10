cd53d743bd3885d507406d9910c5764166a83f3d
Antoan Popov <antoanpopoff@gmail.com>
2022-05-10 18:12:23 +0300

    added initial work for Binance

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useBinanceMarketPrice} from "./api/binance/useBinanceMarketPrice";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const binanceResponse = useBinanceMarketPrice('BTCUSDT1');
+
+
+    return (
+        <div className="App">
+            <div> {!binanceResponse.isLoading && <span>{JSON.stringify(binanceResponse.data)}</span>}</div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/api/constants.ts
@@ -0,0 +1,7 @@
+export enum API_BASE_URL {
+    BINANCE = 'https://api.binance.com/api/v3'
+}
+
+export enum QUERY {
+    BINANCE_GET_TICKER = 'BINANCE_GET_TICKER'
+}
